35a9970ae48b0a982fdf301ad32b8c46445be946
Lesly Castillo <leslymaribel31@gmail.com>
2021-08-15 21:30:45 -0600

    Creacio de base de componentes -Comments-Player-Related

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CommentsComponent } from './comments/comments.component';
+import { RelatedComponent } from './related/related.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentsComponent,
+    RelatedComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
